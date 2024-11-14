b6735fef08277d40523ef357aedc9150184a6504
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-11-14 21:18:29 +0100

    prop in cardList

---
+++ src/components/ProjectCardList.vue
@@ -0,0 +1,26 @@
+<script>
+  export default {
+     data() {
+        return {  
+        }
+  },
+  props:{
+    objectCard:{
+        type: array,
+        required: true
+    }
+  }
+}
+</script>
+
+<template>
+
+   <article class="card d-flex">
+               
+   </article>
+
+</template>
+
+<style lang="scss" scoped>
+ 
+</style>
