5a6fb199dac1758966a0db8ef1ee433a4d2f15c0
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-11-14 17:58:12 +0100

    boostrap and axios

---
+++ src/components/AppMain.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from 'axios';
+
+export default {
+    name: "AppMain",
+    data() {
+        return {
+
+        }
+    },
+    methods:{
+        //metodo para recuperar os projetos 
+        // getProjects(){
+
+        // }
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
