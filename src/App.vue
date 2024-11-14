94a1b6ae5f9815cd7d6fb0200a81be0193588695
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-11-14 18:15:35 +0100

    style in app main

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@ import AppMain from './components/AppMain.vue'
 </script>
 
 <template>
-
+<h1>AppVue</h1>
 </template>
 
-<style scoped>
-
+<style lang="scss">
+@use "./style/general.scss" as *;
+@use "../node_modules/bootstrap/scss/bootstrap.scss" as *;
 </style>
