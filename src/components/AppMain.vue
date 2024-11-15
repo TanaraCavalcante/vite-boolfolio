50a7ed3db6871d9293d656e6a0e4d0f5e53b8891
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-11-15 16:28:31 +0100

    Axios

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,12 +6,25 @@ export default {
     name:"AppMain",  
     data() {
         return {
-        
+            //Preparo per la mia chiamata ajax
+            projectList:[],
+            apiUrl:'http://127.0.0.1:8000/api/projects',
         }
     },
     methods:{
         //Metodo para recuperar meus projetos da API
         getProjects(){
+            axios.get(apiUrl)
+                console.log('Chiamata axios iniziata')
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .finally(function () {
+                    console.log('chiamata axios terminata')
+                });
 
         }
     }
