7981a2756d5ac0925326fec2583a9f0f5e798a8b
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-11-16 07:19:36 +0100

    router in main.js

--- src/js/router.js
+++ src/js/router.js
@@ -1,11 +1,11 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 
-import HomeView from './HomeView.vue'
-import AboutView from './AboutView.vue'
+import AppHome from '../pages/AppHome.vue'
+import AppAbout from '../pages/AppAbout.vue'
 
 const routes = [
-  { path: '/', component: HomeView },
-  { path: '/about', component: AboutView },
+  { path: '/', component: AppHome, name:"home"},
+  { path: '/about', component: AppAbout, name:"about"},
 ]
 
 const router = createRouter({
---
+++ src/pages/AppAbout.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: "AppAbout",
+    data() {
+        return {
+            
+        }
+    },
+    components: {
+        
+    },
+    
+    created() {
+   
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 my-3">
+                <h2>About</h2>
+            </div>
+            <div class="col-12">
+               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis, sequi neque excepturi recusandae voluptatem corrupti aut quo. Eos repudiandae itaque et ex recusandae vero laudantium porro, necessitatibus minus aliquam placeat?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint odio repudiandae omnis doloremque, asperiores obcaecati adipisci, pariatur eos repellendus aliquid perferendis modi perspiciatis tenetur qui dolor, sunt explicabo cupiditate. Dicta?</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
