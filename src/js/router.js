d7e6f29773295153cecaffeec0189247b8333453
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-11-19 20:05:51 +0100

    singleProject in route.js

--- src/js/router.js
+++ src/js/router.js
@@ -3,11 +3,14 @@ import { createMemoryHistory, createRouter } from 'vue-router'
 import AppHome from '../pages/AppHome.vue'
 import AppAbout from '../pages/AppAbout.vue'
 import AppProjects from '../pages/AppProjects.vue'
+import AppSingleProject from '../pages/AppSingleProject.vue'
 
 const routes = [
   { path: '/', component: AppHome, name:"home"},
   { path: '/about', component: AppAbout, name:"about"},
   { path: '/projects', component: AppProjects, name:"projects"},
+  { path: '/projects/:id', component: AppSingleProject, name:"singleProject"},
+
 ]
 
 const router = createRouter({
--- src/pages/AppSingleProject.vue
+++ src/pages/AppSingleProject.vue
@@ -1,15 +1,22 @@
 <script>
+import ProjectCard from '../components/ProjectCard.vue';
+
 export default {
-  data() {
-    return {
-   
+    name:"singleProject",
+    data() {
+        return {
+    
+        }
+    },
+    components:{
+        ProjectCard
     }
-  }
 }
 </script>
 
 <template>
-  
+    <h2>Single card</h2>
+    <ProjectCard/>
 </template>
 
 <style scoped>
