337c695546d41eb325242d67de533e640e37f58a
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-11-19 16:48:08 +0100

    css loader

--- src/components/AppLoader.vue
+++ src/components/AppLoader.vue
@@ -10,11 +10,17 @@ export default {
 </script>
 
 <template>
-  <h1>
-    loading
-  </h1>
+    <!-- credits https://css-loaders.com/dots/ -->
+ <div class="loader"></div>
 </template>
 
 <style scoped>
-
+.loader {
+  width: 60px;
+  aspect-ratio: 4;
+  background: radial-gradient(circle closest-side,#000 90%,#0000) 0/calc(100%/3) 100% space;
+  clip-path: inset(0 100% 0 0);
+  animation: l1 1s steps(4) infinite;
+}
+@keyframes l1 {to{clip-path: inset(0 -34% 0 0)}}
 </style>
